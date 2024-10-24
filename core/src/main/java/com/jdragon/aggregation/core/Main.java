7ba7269312d532d0ff8776c3784c44912337858c
jdragon <jdraong9527@gmail.com>
2024-10-24 10:07:58 +0800

    feat: test

--- core/src/main/java/com/jdragon/aggregation/core/Main.java
+++ core/src/main/java/com/jdragon/aggregation/core/Main.java
@@ -2,31 +2,30 @@ package com.jdragon.aggregation.core;
 
 import com.alibaba.fastjson.JSONObject;
 import com.jdragon.aggregation.commons.pagination.Table;
+import com.jdragon.aggregation.commons.util.Configuration;
 import com.jdragon.aggregation.datasource.AbstractDataSourcePlugin;
 import com.jdragon.aggregation.datasource.BaseDataSourceDTO;
 import com.jdragon.aggregation.datasource.SourcePluginType;
 import com.jdragon.aggregation.pluginloader.PluginClassLoaderCloseable;
 
+import java.io.File;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
-        try (PluginClassLoaderCloseable classLoaderSwapper = PluginClassLoaderCloseable.newCurrentThreadClassLoaderSwapper(SourcePluginType.SOURCE, "tbds-hive2")) {
-            AbstractDataSourcePlugin mysql5 = classLoaderSwapper.loadPlugin();
-            BaseDataSourceDTO mysqlDto = new BaseDataSourceDTO();
-            mysqlDto.setName("mysql5");
-            mysqlDto.setHost("rmHost");
-            mysqlDto.setPort("3304");
-            mysqlDto.setDatabase("ag");
-            mysqlDto.setUserName("root");
-            mysqlDto.setPassword("951753");
-            mysqlDto.setUsePool(true);
 
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("useSSL", "false");
-            mysqlDto.setOther(JSONObject.toJSONString(jsonObject));
-            Table<Map<String, Object>> agUser = mysql5.dataModelPreview(mysqlDto, "ag_user", "10");
-            System.out.println(agUser);
+    private static final String configPath = System.getProperty("configPath", "source.json");
+
+    private static final String execSql = System.getProperty("execSql");
+
+    private static final Configuration configuration = Configuration.from(new File(configPath));
+
+    public static void main(String[] args) {
+        String json = configuration.toJSON();
+        BaseDataSourceDTO dto = JSONObject.parseObject(json, BaseDataSourceDTO.class);
+        try (PluginClassLoaderCloseable classLoaderSwapper = PluginClassLoaderCloseable.newCurrentThreadClassLoaderSwapper(SourcePluginType.SOURCE, dto.getName())) {
+            AbstractDataSourcePlugin mysql5 = classLoaderSwapper.loadPlugin();
+            Table<Map<String, Object>> mapTable = mysql5.executeQuerySql(dto, execSql, true);
+            System.out.println(mapTable);
         }
     }
 }
